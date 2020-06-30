1f3850d25c018cfd4eae620871cc672898b6ea47
Gregory A.S. Wright <geslim@gmail.com>
2020-06-30 17:47:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def square_array(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map do |element|
+  planeteer_calls.all? do |element|
     puts "#{element.capitalize()}!"
   end
 end
