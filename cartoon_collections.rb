348be6a7837604b7a75231a0f2ab7b47e42d31f4
Gregory A.S. Wright <geslim@gmail.com>
2020-06-30 18:39:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |element|
-    valid_calls
+    element
   end
 end
